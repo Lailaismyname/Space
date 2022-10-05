2b836b724b3e93cec532ef1dae3fc97e78359d1c
Laila ofzo <mynameislaila@outlook.com>
2022-10-05 22:25:44 +0200

    oke

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 //api key = dZvFSDlqyjb3S5prOv06EbmfM5splAc1vjL0hUIq
 
+let asteroidIndex = 0;
+
 document.querySelector('#dateBtn').addEventListener('click', getFetch);
 
 
@@ -10,8 +12,9 @@ function getFetch(){
     fetch(url)
         .then(res => res.json())
         .then(data => {
-            let asteroidIndex = 0;
-                        
+            console.log(data);
+            
+            const outputData = document.querySelector('#outputData');
             
             const elementCount = data.element_count;
             const aboluteMagnitude = data['near_earth_objects'][datum][asteroidIndex]['absolute_magnitude_h'];
@@ -21,21 +24,13 @@ function getFetch(){
             const diameterKilMin = data['near_earth_objects'][datum][asteroidIndex]['estimated_diameter']['kilometers']['estimated_diameter_min'];
             const diameterKilMax = data['near_earth_objects'][datum][asteroidIndex]['estimated_diameter']['kilometers']['estimated_diameter_max'];
             const objectName = data['near_earth_objects'][datum][asteroidIndex]['name'];
-            console.log(data);
+            
             document.querySelector('#numAst').max= elementCount;
-
             document.querySelector('#elCount').innerHTML = `We have detected ${elementCount} nearby asteroids for this search. Select which asteroid data you wish to view`;
             //gebruiker moet een object selecteren en dan is het de data hieronder die daar betrekking op heeft
 
-            document.querySelector('#objectName').innerHTML = `Detected ${objectName}`;
+            outputData.innerHTML = `We have detected ${objectName}. It was orbitting ${celBody}. The distance from earth was ${earthDistKil} kilometers. It was traveling at a velocity of ${velocityKmPerHour} kilometers per hour. It has a minimum diameter of ${diameterKilMin} Kilometers and a maximum diameter of ${diameterKilMax}. The absolute magnitude of this object was ${aboluteMagnitude}`
 
-            document.querySelector('#orbit').innerHTML = `It was orbitting ${celBody}`;
-            document.querySelector('#earthDist').innerHTML = `The distance from earth was ${earthDistKil} kilometers`;
-            document.querySelector('#velocity').innerHTML = `It was traveling at a velocity of ${velocityKmPerHour} kilometers per hour`;
-            document.querySelector('#diameter').innerHTML = `It has a minimum diameter of ${diameterKilMin} Kilometers and a maximum diameter of ${diameterKilMax}`;
-            document.querySelector('#abMag').innerHTML = `The absolute magnitude of this object was ${aboluteMagnitude}`;
-            //document.querySelector('').innerHTML = ;
-            //document.querySelector('').innerHTML = ;
             
         })
         .catch(err => {
@@ -45,12 +40,8 @@ function getFetch(){
 
 let showButton = document.querySelector('#showOtherAsteroid');
             showButton.addEventListener('click',()=>{
-                asteroidIndex = document.querySelector('#numAst').value;
+                asteroidIndex = document.querySelector('#numAst').value - 1;
                 getFetch(asteroidIndex);
                 console.log(asteroidIndex);
             })
-//Oke selectere op datum werkt, nu nog toevoegen dat je door alle objecten heen kan klikken.
-//next en previous button toevoegen, plus selecteren op getal. 
-//en dan opmaak even mooi maken
-
 //picture of the day toevoegen met ander api. 
